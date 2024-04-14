edf040f9472627aabb69d75df1b124b93c5882eb
prashantmane17 <prashantmn47@gmail.com>
2024-04-14 16:59:34 +0530

    update6.3

--- Pages/Contact Us/Script.js
+++ Pages/Contact Us/Script.js
@@ -1,9 +1,10 @@
+
+
 const loader=document.querySelector('#loader');
 
 window.addEventListener('load',()=>{
     loader.style.display="none";
 })
-
 
 const observer =new IntersectionObserver((entries)=> {
     entries.forEach((entry)=>{
